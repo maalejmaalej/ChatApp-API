01a74a78a822058766201483f354dc1294a8145f
maalejmaalej <56475552+maalejmaalej@users.noreply.github.com>
2021-09-06 01:12:19 +0100

    fix bugs

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,11 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
-import { WsAdapter } from '@nestjs/platform-ws';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
   app.enableCors();
   app.useGlobalPipes(new ValidationPipe())
-  await app.listen(8000);
+  await app.listen(process.env.PORT || 8000);
 }
 bootstrap();
